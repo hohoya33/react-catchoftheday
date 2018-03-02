09d708ceb7e8ea6afda204599280bb3b36d486cb
ho <hohoya33@gmail.com>
2018-03-02 15:44:11 +0900

    #13 Understanding State Inventory에서 입력 받은 값들을 App.js에서 this.state.fishes에 추가

--- catchoftheday-app/src/components/App.js
+++ catchoftheday-app/src/components/App.js
@@ -4,6 +4,18 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends Component {
+    state = {
+        fishes: {},
+        order: {}
+    }
+    addFish = fish => {
+        // 1. Take a copy of the existing state
+        const fishes = { ...this.state.fishes };
+        // 2. Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set the new fishes object to state
+        this.setState({ fishes });
+    }
     render() {
         return (
             <div className="catch-of-the-day">
@@ -12,7 +24,7 @@ class App extends Component {
                 </div>
 
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
